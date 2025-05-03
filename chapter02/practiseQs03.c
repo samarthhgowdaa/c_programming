d6e11f01ebce913dda06b42160f895e30db64cf8
samarthhgowdaa <samarthac4work@gmail.com>
2025-05-04 00:29:50 +0530

    sssss

---
+++ chapter02/practiseQs03.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    
+    int a = (int) 1.99999; //to force it to out a decimal
+    printf("%d", a);
+
+    return 0;
+}
---
+++ chapter02/typeConversion.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+    //if %d is used then the value will be zero, same with others
+    printf("%f", 2.0 * 2);
+    return 0;
+}
