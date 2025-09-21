25c879f6492bef406e934dd3dfa2762b06e0bdfd
samarthhgowdaa <samarthac4work@gmail.com>
2025-09-21 04:59:51 +0000

    A folder was created, Started learning C from BroCodes

---
+++ broCodes/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    //My not so first attempt in C
+
+    /*
+    This is my 2nd try 
+    on C programming, after not so completing
+    the apna college tutorial
+    */
+   
+    printf("Hello World!\n");
+    printf("I'm SA30!");
+
+    return 0;
+}
